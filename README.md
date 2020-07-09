290348ab420fe7470ec0cf34a04312476e2a3ac3
RyanxLoi <=>
2020-07-09 13:00:24 -0400

    new version

---
+++ README.md
@@ -0,0 +1,131 @@
+# The Brant Steele Hunger Games Generator Script
+Given two csv files, this script can quickly generate a simulated Hunger Game using Brant Steele's generator. 
+
+This script was specifically created for a Discord server, but can be modified as necessary to suit your needs. 
+
+# Prerequisites
+
+In order to use this script, you'll need:
+
+* FireFox or Google Chrome: [Download Google Chrome here](https://www.google.com/chrome/) OR [Download Firefox here](https://www.mozilla.org/en-CA/firefox/)
+* The web driver for your preferred browser above: [ChromeDriver for Google Chrome](https://chromedriver.chromium.org/) OR [geckodriver for FireFox](https://github.com/mozilla/geckodriver/releases)
+* Selenium WebDriver: [Download here](https://www.selenium.dev/)
+
+Ensure that your browser drivers are placed in your PATH in order for the script to work. 
+
+# Downloading and Set Up
+
+To download, you may simply download the repository as a zip folder or clone the repository using git. 
+
+Once downloaded, you'll have to make some modifications to `cast.csv`, `events.csv` and `creategame.py`.
+
+Do not change the names of the above files as they are referenced in the code. 
+
+## cast.csv
+
+cast.csv is your file that contains information on your tributes. It contains the follow headers:
+
+* Timestamp	
+* Name	
+* Discord Name
+* Pronouns	
+* Profile pic link	
+* Event
+
+If you have created a Google form that people can use to apply to your Hunger Games, you can simply export the results to this folder and rename it as cast.csv. 
+
+## events.csv
+
+events.csv contains custom events that may occur during your game. It has the following headers:
+
+* Type	
+* Fatal	
+* NumberTrib	
+* T1Killer	
+* T2Killer	
+* T3Killer	
+* T4Killer	
+* T5Killer	
+* T6Killer	
+* T1Killed	
+* T2Killed	
+* T3Killed	
+* T4Killed	
+* T5Killed	
+* T6Killed	
+* Event
+
+### Type
+
+The type refers to when the event occurs. It can occur during one of the following times of the day:
+
+* Bloodbath
+* Day
+* Night
+* Feast
+
+**NOTE:** At this time, it cannot support arena events. 
+
+### Fatal
+
+Fatal determines if the event results in at least one tribute being eliminated from the game. The cells in the csv are either marked with a "Y" for yes (fatal) or "N" for no (not fatal).
+
+### NumTributes
+
+This refers to how many tributes are involved in an event. It can range from 1 to 6 inclusive. 
+
+### T1-T6 Killer and Killed
+
+These columns determine which players are the killers and which players are killed. Cells in this column are marked with either a 0 for "No" (Player is not the killer and/or not killed) and 1 for "Yes" (Player is the killer and/or not killed). Events where players are marked as a killer will have their kill count increase by 1 when the event occurs.   
+
+### Event
+
+This is the description of the event itself. 
+
+## creategame.py
+
+This is the script itself. The follow modifications can be made to the script:
+
+### What Browser to Use
+
+You have the option to create the game using either Firefox or Google Chrome. To select one of the browsers, comment out the code of the browser that you are not using in the code block below in the file. 
+
+```
+    #Create firefox profile with preferences to disable loading images
+    #Disabling images allows the page to load faster
+    firefox_profile = webdriver.FirefoxProfile()
+    firefox_profile.set_preference('permissions.default.image', 2)
+    firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
+    #create Firefox Driver
+    driver = webdriver.Firefox(firefox_profile=firefox_profile)
+
+
+    #Create chrome profile with preferences to disable loading images
+    #Disabling images allows the page to load faster
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+    #create Chrome driver
+    driver = webdriver.Chrome("/path/to/chromedriver",chrome_options=chrome_options)
+```
+
+In the line ```driver = webdriver.Chrome("/path/to/chromedriver",chrome_options=chrome_options)```, replace the /path/to with the actual path of your chromedriver.
+
+### Season Name and Season Url
+
+These are fields that label and represent your game.
+
+``` 
+    seasonName = driver.find_element_by_name("seasonname")
+    seasonName.send_keys("Hunger Games")
+    logoUrl = driver.find_element_by_name("logourl")
+    logoUrl.send_keys("https://upload.wikimedia.org/wikipedia/en/d/dc/The_Hunger_Games.jpg")
+```
+
+In this block above, you may change the name of the season in the quotaton marks inside `seasonName,send_keys` and the url of the logo for your game in the quotation marks inside `logoUrl.send_keys`.
+
+# Usage
+
+When everything has been set up, in your terminal, go into the folder and type in `python creategame.py`. From there, you'll be prompted to select to create a game with either 24, 36 or 48 tributes and you are good to go.
+
+Once your game has been created, you can obtain the url provided by the generator and share it!
---
+++ creategame.py
@@ -0,0 +1,213 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.firefox.options import Options
+
+import csv
+
+url = "https://brantsteele.net/hungergames/disclaimer.php"
+
+def create(numTributes):
+
+    #############################################################
+    #
+    #This script is currently supported on Firefox and Google Chrome. Select your preferred browser and disable the other one by #commenting out the code of the other browser below before the next line of hashtags. From my personal experience, Firefox #performs much faster than Chrome but Chrome is still an option.
+    #
+    ##############################################################
+    
+    #Create firefox profile with preferences to disable loading images
+    #Disabling images allows the page to load faster
+    options = Options()
+    options.add_argument('-headless')
+    firefox_profile = webdriver.FirefoxProfile()
+    firefox_profile.set_preference('permissions.default.image', 2)
+    firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
+    #create Firefox Driver
+    driver = webdriver.Firefox(firefox_profile=firefox_profile,options=options)
+
+
+    #Create chrome profile with preferences to disable loading images
+    #Disabling images allows the page to load faster
+    #chrome_options = webdriver.ChromeOptions()
+    #chrome_options.add_experimental_option("detach", True)
+    #prefs = {"profile.managed_default_content_settings.images": 2}
+    #chrome_options.add_experimental_option("prefs", prefs)
+    #create Chrome driver
+    #driver = webdriver.Chrome("/path/to/chromedriver",chrome_options=chrome_options)
+
+    driver.implicitly_wait(20)
+    driver.maximize_window()
+
+    ##############################################################################################
+
+    #Start process
+    driver.get(url)
+
+    #Agree to TOC
+    agreeLink = driver.find_element_by_link_text("I am 13 years or older. I have read and understand these terms.")
+    agreeLink.click()
+
+    #Change cast size if user has selected 36 or 48 tributes
+    if numTributes == 36 or numTributes == 48:
+        adjustSize = driver.find_element_by_link_text("Adjust Size")
+        adjustSize.click()
+        if numTributes == 36:
+            newSize = driver.find_element_by_link_text("Use 36 tributes.")
+            newSize.click()
+        elif numTributes == 48:
+            newSize = driver.find_element_by_link_text("Use 48 tributes.")
+            newSize.click()
+
+    #Begin creating the game
+    hoverLink = driver.find_element_by_link_text("Simulate")
+    hover = ActionChains(driver).move_to_element(hoverLink)
+    hover.perform()
+    driver.implicitly_wait(10)
+
+    createPersonal = driver.find_element_by_link_text("Personal")
+    createPersonal.click()
+
+    driver.implicitly_wait(10)
+
+    #For send_keys, you may replace the content inside the quotation marks with another name for your season and another url for your logo
+    seasonName = driver.find_element_by_name("seasonname")
+    seasonName.send_keys("Hunger Games")
+    logoUrl = driver.find_element_by_name("logourl")
+    logoUrl.send_keys("https://upload.wikimedia.org/wikipedia/en/d/dc/The_Hunger_Games.jpg")
+
+    cast = []
+
+    #read csv for cast
+    with open('cast.csv', newline='') as castFile:
+        castReader = csv.reader(castFile)
+        for row in castReader:
+            cast.append(row)
+
+    #[Timestamp,Name,Discord Name,Pronouns,Profile pic link,Events]
+
+    #Create cast
+    for i in range(1,numTributes+1):
+        if i < 10:
+            num = "0" + str(i)
+        else:
+            num = str(i)
+        tribName = "cusTribute" + num
+        tribNick = tribName + "nickname"
+        tribImg = tribName + "img"
+        tribGender = tribName + "gender"
+
+        driver.find_element_by_name(tribName).send_keys(cast[i][0])
+        driver.find_element_by_name(tribNick).send_keys(cast[i][1])
+        driver.find_element_by_name(tribImg).send_keys(cast[i][3])
+
+        selectGender = Select(driver.find_element_by_name(tribGender))
+        if cast[i][2] == "He/Him":
+            selectGender.select_by_index("1")
+        else:
+            selectGender.select_by_index("2")
+
+    bwBtn = driver.find_element_by_class_name("MakeEvictedBW")
+    bwBtn.click() 
+
+    sameNickBtn = driver.find_element_by_class_name("MakeEvictedNames")
+    sameNickBtn.click()
+
+    submitBtn = driver.find_element_by_xpath("//input[@type='submit' and @value='Submit']")
+    submitBtn.click()
+    print("Cast created.")
+    #Create events
+
+    events = []
+
+    #read csv for events
+    with open('events.csv', newline='') as eventsFile:
+        eventsReader = csv.reader(eventsFile)
+        for row in eventsReader:
+            events.append(row)
+
+    modifyEvents = driver.find_element_by_link_text("Modify Events")
+    modifyEvents.click()
+    
+    #['Type', 'Fatal', 'NumberTrib', 'T1Killer', 'T2Killer', 'T3Killer', 'T4Killer', 'T5Killer', 'T6Killer', 'T1Killed', 'T2Killed', 'T3Killed', 'T4Killed', 'T5Killed', 'T6Killed', 'Event']
+
+    for j in range(1,len(events)):
+        eventType = events[j][0].lower()
+        killerCol = 3
+        killedCol = 9
+        
+        if events[j][1] == "N":
+            driver.get("https://brantsteele.net/hungergames/AddEvent.php?type="+eventType)
+            numTribsSelect = Select(driver.find_element_by_name("EventNumber"))
+            numTribsEvent = events[j][2]
+            numTribsSelect.select_by_value(str(numTribsEvent))
+            driver.find_element_by_name("EventText").send_keys(events[j][15])
+            driver.find_element_by_xpath("//input[@type='submit' and @value='Submit']").click()
+        elif events[j][1] == "Y":
+            driver.get("https://brantsteele.net/hungergames/AddEvent.php?type="+eventType+"fatal")
+            numTribsSelect = Select(driver.find_element_by_name("EventNumber"))
+            numTribsEvent = events[j][2]
+            numTribsSelect.select_by_value(str(numTribsEvent))
+
+            for trib in range(1,(int(numTribsEvent)+1)):
+                if events[j][killerCol] != "-" or events[j][killedCol] != "-":
+                    killerSelect = Select(driver.find_element_by_name("killer"+str(trib)))
+                    killedSelect = Select(driver.find_element_by_name("killed"+str(trib)))
+                    killerSelect.select_by_value(events[j][killerCol])
+                    killedSelect.select_by_value(events[j][killedCol])
+                    killerCol += 1
+                    killedCol += 1
+                else:
+                    continue
+            
+            driver.find_element_by_name("EventText").send_keys(events[j][15])
+            driver.find_element_by_xpath("//input[@type='submit' and @value='Submit']").click()
+
+        driver.get("https://brantsteele.net/hungergames/ModifyEvents.php")
+    
+    print("Events added.")
+
+    #Generate game and get link
+    driver.find_element_by_link_text("Save").click()
+    driver.find_element_by_link_text("Yes").click()
+
+    gameUrl = driver.find_element_by_xpath("/html/body/div/div[5]/div[4]/a[1]").text
+
+    print("Game saved successfully! Obtain the url to the game provided by the simulator and enjoy! :)")
+    print("Your game link is: " + gameUrl)
+    
+ 
+def main():
+    print("Input '1' for 24 tributes")
+    print("Input '2' for 36 tributes")
+    print("Input '3' for 48 tributes")
+
+    numTributes = 0
+    validOption = False
+
+    while numTributes < 1 or numTributes > 3 and validOption == False:
+        try:
+            numTributes = int(input("Select number of tributes: "))
+            if numTributes == 1:
+                numTributes = 24
+                validOption = True
+            elif numTributes == 2:
+                numTributes = 36
+                validOption = True
+            elif numTributes == 3:
+                numTributes = 48
+                validOption = True
+            if validOption == True:
+                break
+            else:
+                print("Invalid input. Input 1, 2 or 3.")
+        except ValueError:
+            print("Invalid input. Input 1, 2 or 3.")
+    print("Please wait. It should not take too long.")
+    create(numTributes)
+
+main()
